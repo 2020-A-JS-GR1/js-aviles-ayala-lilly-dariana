16574c79e0a6696dae8f4aa8a9148158e57d82fe
Lilly Aviles <lilly.aviles@epn.edu.ec>
2020-06-12 17:42:35 -0500

    Truty Falsy " " 0

--- 01-javascript/variables.js
+++ 01-javascript/variables.js
@@ -19,7 +19,7 @@ const configuracionArchivo ="PDF";
 
 const numero =1; //number
 const sueldo = 1.2; //number
-const texto = "Pamela"; // string
+const texto = "Lilly"; // string
 const booleanoo = false; // boolean
 const hijos = null; // object
 const zapatos = undefined; // undefined
@@ -28,6 +28,48 @@ const zapatos = undefined; // undefined
 console.log(typeof numero);
 console.log(typeof sueldo);
 console.log(typeof texto);
-console.log(typeof booleanoo);
+console.log(typeof booleanooo);
 console.log(typeof hijos);
+console.log(typeof numero);
 console.log(typeof zapatos);
+console.log(typeof apellido);
+console.log(typeof Number("asd"));
+console.log(Number("asd"));
+
+if(true == true){
+    console.log("Es verdadero"); //!
+}else{
+    console.log("Es Falso");
+}
+
+if(true == false){
+    console.log("Es verdadero");
+}else{
+    console.log("Es Falso"); //!
+}
+
+if(""){
+    console.log("Es verdadero");
+}else{
+    console.log("Es Falsy"); //!
+}
+if("Adrian"){
+    console.log("Es truty");
+}else{
+    console.log("Es Falso");
+}
+if(-1){
+    console.log("Es truty"); // Truty
+}else{
+    console.log("Es Falso");
+}
+if(0){
+    console.log("Es truty");
+}else{
+    console.log("Es Falso"); // Truty
+}
+if(1){
+    console.log("Es truty"); // Falsy
+}else{
+    console.log("Es Falso");
+}}
