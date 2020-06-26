f95b96bb29678175045e7f98690637b725ccdf07
Lilly Aviles <lilly.aviles@epn.edu.ec>
2020-06-26 17:21:17 -0500

    Destructuración e inicio de callbacks

--- 01-javascript/05-destructuration.js
+++ 01-javascript/05-destructuration.js
@@ -4,5 +4,24 @@ const adrian = {
 const carolina = {
     apellido: "Eguez"
 };
-const arregloUno = [1, 2, 3, 4, 5];
-const arregloDos = [6, 7, 8, 9, 10];
+//const arregloUno = [1, 2, 3, 4, 5];
+//const arregloDos = [6, 7, 8, 9, 10];
+
+const adrianCarolina = { //Creando una nueva referencia
+    ...adrian,
+    ...carolina,
+};
+adrianCarolina.nombre="Carolina";
+console.log('adrianCarolina',adrianCarolina);
+console.log('adrian',adrian);
+
+
+const arregloUno = [1,2,3,4,5];
+const arregloDos = [1,2,3,4,5];
+const superArreglo=[
+    ...arregloUno,
+    ...arregloDos
+];
+superArreglo[0]= 100;
+console.log('superArreglo',superArreglo);
+console.log('superUno',arregloUno);
